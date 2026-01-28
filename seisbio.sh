c5f690206181ba587a73e41b35a423b73e6ff43f
Alejandro-Estrada-1 <alejandro.sanchez.estrada@ciencias.unam.mx>
2026-01-28 09:46:31 -0600

    Directories

--- seisbio.sh
+++ seisbio.sh
@@ -4,7 +4,7 @@ SEISBIO_HOME="/home/seisbio"
 
 if [[ "$(pwd)" != "$SEISBIO_HOME" ]]; then
     echo "[INFO] Copiando archivos..."
-    sudo cp virtual_envs.txt small_virtual_envs.txt build_container.sh create_container.sh "$SEISBIO_HOME/"
+    sudo cp envs/virtual_envs.txt envs/small_virtual_envs.txt build_container.sh create_container.sh install_seisbio.sh "$SEISBIO_HOME/"
 
     echo "[INFO] Abriendo sesión como seisbio..."
     exec sudo -i -u seisbio
